7b4c207da58617ff5547946334c23a32c1a3845f
unfinishedmakerspace <142112870+unfinishedmakerspace@users.noreply.github.com>
2024-12-04 18:16:31 -0800

    mask on picture

--- Picturesensor.py
+++ Picturesensor.py
@@ -3,11 +3,20 @@ import cv2
 image = cv2.imread("C:\\Users\\maker\\Pictures\\IMG_6442.png")
 print(type(image))
 Window = 'window'
+Mask = 'mask'
 cv2.namedWindow(Window)
+cv2.namedWindow(Mask)
 height, width = image.shape[:2]
-width = width/2
-height = height/2
-resized_image = cv2.resize(image, (width/2, height/2), interpolation = cv2.INTER_CUBIC)
+image = cv2.resize(image, (int(0.1*width), int(0.1*height)), interpolation=cv2.INTER_AREA)
+low_H = 30
+low_S = 70
+low_V = 0
+high_H = 40
+high_S = 255
+high_V = 255
+frame_threshold = cv2.inRange(image, (low_H, low_S, low_V), (high_H, high_S, high_V))
+
 cv2.imshow(Window, image)
+cv2.imshow(Mask, frame_threshold)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
