8b834a8e4f51387e9d3b6fa25abcfec0d172a1f0
topspin10 <andrewH7wei@gmail.com>
2024-12-30 17:08:56 -0800

    used dilation, cleaned up, starting next step

--- Directionprovider.py
+++ Directionprovider.py
@@ -1,8 +1,5 @@
 import cv2
 image = cv2.imread("C:\\Users\\maker\\Documents\\GitHub\\ball_picker_robot\\pictures\\IMG_6443.png")
-# C:\Users\unfin\OneDrive\Documents\GitHub\ball_picker_robot\pictures\IMG_6442.png"
-#"C:\\Users\\maker\\Documents\\GitHub\\ball_picker_robot\\pictures\\IMG_6442.png"
-#image = cv2.imread("C:\\Users\\maker\\Pictures\\IMG_6442.png")
 print(type(image))
 low_H_name = 'Low H'
 low_S_name = 'Low S'
@@ -52,6 +49,9 @@ def on_high_V_thresh_trackbar(val):
     high_V = max(high_V, low_V+1)
     cv2.setTrackbarPos(high_V_name, Mask, high_V)
 
+def opencv_based_ball_detector(image):
+    
+
 cv2.namedWindow(findcontour)
 cv2.namedWindow(Window)
 cv2.namedWindow(Mask)
@@ -67,16 +67,6 @@ high_V = 170
 erosion_shape = cv2.MORPH_RECT
 erosion_size = 1
 kernel = cv2.getStructuringElement(erosion_shape, (2 * erosion_size + 1, 2 * erosion_size + 1),(erosion_size, erosion_size))
-# title_trackbar_element_shape = 'Element:\n 0: Rect \n 1: Cross \n 2: Ellipse'
-# frame_threshold = cv2.inRange(image, (low_H, low_S, low_V), (high_H, high_S, high_V))
-# cv2.imshow(Mask, frame_threshold)
-# src = "C:\\Users\\maker\\Documents\\GitHub\\ball_picker_robot\\pictures\\IMG_6443.png"
-# erosion_size = 0
-# max_elem = 2
-# max_kernel_size = 21
-# title_trackbar_element_shape = 'Element:\n 0: Rect \n 1: Cross \n 2: Ellipse'
-# title_trackbar_kernel_size = 'Kernel size:\n 2n +1'
-# element =
 
 cv2.createTrackbar(low_H_name, Mask, low_H, 188, on_low_H_thresh_trackbar)
 cv2.createTrackbar(high_H_name, Mask, high_H, 188, on_high_H_thresh_trackbar)
@@ -84,30 +74,10 @@ cv2.createTrackbar(low_S_name, Mask, low_S, 255, on_low_S_thresh_trackbar)
 cv2.createTrackbar(high_S_name, Mask, high_S, 255, on_high_S_thresh_trackbar)
 cv2.createTrackbar(low_V_name, Mask, low_V, 255, on_low_V_thresh_trackbar)
 cv2.createTrackbar(high_V_name, Mask, high_V, 255, on_high_V_thresh_trackbar)
-# cv2.createTrackbar(title_trackbar_element_shape, Erode, 0, max_elem, erosion)
-
 
 while True:
     frame_threshold = cv2.inRange(image, (low_H, low_S, low_V), (high_H, high_S, high_V))
     cv2.imshow(Mask, frame_threshold)
-    # def morph_shape(val):
-    #     if val == 0:
-    #         return cv2.MORPH_RECT
-    #     elif val == 1:
-    #         return cv2.MORPH_CROSS
-    #     elif val == 2:
-    #         return cv2 .MORPH_ELLIPSE
-    # def erosion(val):
-    #     erosion_size = cv2.getTrackbarPos(title_trackbar_kernel_size, Erode)
-    #     erosion_shape = morph_shape(cv2.getTrackbarPos(title_trackbar_element_shape, Erode))
-    #
-    #     element = cv2.getStructuringElement(erosion_shape, (2 * erosion_size + 1, 2 * erosion_size + 1),
-    #                                        (erosion_size, erosion_size))
-    #
-    #     erosion_dst = cv2.erode(src, element)
-    #     cv2.imshow(Erode, erosion_dst)
-
-    # erosion("C:\\Users\\maker\\Documents\\GitHub\\ball_picker_robot\\pictures\\IMG_6443.png")
 
 
 
@@ -118,9 +88,7 @@ while True:
     cv2.drawContours(image, contours, -1, (0, 255, 0), -1)
     cv2.imshow(findcontour, image)
 
-    # cv2.imshow(findcontour, contour)
     key = cv2.waitKey(0)
     if key == ord('q') or key == 27:
         break
-# cv2.waitKey(0)
 cv2.destroyAllWindows()
