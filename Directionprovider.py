a13c328bdbf0d12fb5d57ffd7f9ccd3f28a7adcb
topspin10 <andrewH7wei@gmail.com>
2025-01-06 18:24:53 -0800

    working on 2nd function

--- Directionprovider.py
+++ Directionprovider.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 def color_based_ball_detector(image):
     '''input = image
@@ -16,3 +17,18 @@ def color_based_ball_detector(image):
     output_2 = cv2.erode(output_1, kernel)
     output_3 = cv2.dilate(output_2, kernel, iterations=10)
     return output_3
+
+def circle_based_ball_detector(image):
+    '''input = image
+    output = black and white version of the image'''
+    input = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    output_1 = cv2.HoughCircles(input, cv2.HOUGH_GRADIENT, 2, 200, minRadius=50, maxRadius=500)
+    # measured thing. closest ball diameter is 500
+    if output_1 is not None:
+        output_1 = np.uint16(np.around(output_1))
+        for i in output_1[0, :]:
+            center = (i[0], i[1])
+            radius = i[2]
+            cv2.circle(image, center, radius, (255, 255, 255), -1)
+
+    return image
--- test.py
+++ test.py
@@ -1,12 +1,16 @@
 import numpy as np
 import cv2 as cv
-import Directionprovider
+import Directionprovider as dp
 
 newimage = 'newimage'
 cv.namedWindow(newimage)
 image = cv.imread("C:\\Users\\maker\\Documents\\GitHub\\ball_picker_robot\\pictures\\IMG_6443.png")
-output = Directionprovider.color_based_ball_detector(image)
-output = cv.resize(output, None, fx=0.1, fy=0.1)
-cv.imshow(newimage, output)
+# output_1 = dp.color_based_ball_detector(image)
+# output_1 = cv.resize(output_1, None, fx=0.1, fy=0.1)
+# cv.imshow(newimage, output_1)
+image = cv.resize(image, None, fx=0.1, fy=0.1)
+output_2 = dp.circle_based_ball_detector(image)
 
-key = cv.waitKey(0)
+cv.imshow(newimage, output_3)
+
+# key = cv.waitKey(0)
