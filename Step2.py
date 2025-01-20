456719076b274b0db2185411786f8de0cf1f2b08
topspin10 <andrewH7wei@gmail.com>
2025-01-19 20:46:09 -0800

    Update Step2.py

--- Step2.py
+++ Step2.py
@@ -8,10 +8,11 @@ import numpy as np
 
 def direction_decider(image):
     """in: output of step 1 - out: number between -1 and 1"""
-    #vimage = cv.resize(image, None, fx=0.1, fy=0.1)
-    output_1 = 0
-    top_bob = 0
-    check = 0
+    # image = cv.resize(image, None, fx=0.1, fy=0.1)
+    w_bins = 10
+    h_bins
+    output_1 = (w_bins-1)/2
+    top_slice = 0
     threshold = 100
     # cv.namedWindow('full image')
     # cv.imshow('full image', image)
@@ -19,24 +20,20 @@ def direction_decider(image):
     # print(image)
     # cv.namedWindow('part image')
     height, length = image.shape
-    height = height // 2
+    height = height // h_bins
     image_2 = image[height:]
-    length = length // 10
+    length = length // w_bins
     output = 0
-    for bob in range(10):
-        image_3 = image_2[:, length * bob:length * (bob + 1)]
-        number = bob
+    for slice in range(w_bins):
+        image_3 = image_2[:, length * slice:length * (slice + 1)]
         white_pixel = np.sum(image_3 == 255)
-        if top_bob < white_pixel:
+        if top_slice < white_pixel:
             if white_pixel >= threshold
-                top_bob = white_pixel
-                output_1 = number
-                check = 1
-if check == 0:
-    output = 1
+                top_slice = white_pixel
+                output_1 = slice
 
         # cv.imshow('part image', image_3)
-        cv.waitKey(0)
+        # cv.waitKey(0)
 
-    output_2 = output_1/(9/2)-1
+    output_2 = output_1/((w_bins-1)/2)-1
     return output_2
