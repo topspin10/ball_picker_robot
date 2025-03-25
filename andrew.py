02756923e81b0665265bf7eff826a547716d52ab
unfinishedmakerspace <142112870+unfinishedmakerspace@users.noreply.github.com>
2025-03-24 18:24:31 -0700

    finished copying over code.

--- andrew.py
+++ andrew.py
@@ -1,6 +1,6 @@
 import numpy as np
-# import matplotlib.pyplot as plt
-# import os
+import matplotlib.pyplot as plt
+import os
 from PIL import Image
 # import keras
 # from keras.models import Model
@@ -15,9 +15,11 @@ from PIL import Image
 # import glob
 # import random
 # import cv2
-# from random import shuffle
+from random import shuffle
 
 
+# this function provides examples for my model, so I can train it.
+# my model needs examples to be able to replicate it when it is given new input.
 def image_generator(files, batch_size=32, sz=(256, 256)):
     while True:
 
@@ -55,11 +57,47 @@ def image_generator(files, batch_size=32, sz=(256, 256)):
             batch_x.append(raw)
 
         # preprocess a batch of images and masks
+        # creates n
         batch_x = np.array(batch_x) / 255.
         batch_y = np.array(batch_y)
         batch_y = np.expand_dims(batch_y, 3)
 
-        yield (batch_x, batch_y)
+        yield batch_x, batch_y
+
+
+batch_size = 32
+
+all_files = os.listdir('images')
+shuffle(all_files)
+
+split = int(0.95 * len(all_files))
+
+# split into training and testing
+train_files = all_files[0:split]
+test_files = all_files[split:]
+
+train_generator = image_generator(train_files, batch_size=batch_size)
+test_generator = image_generator(test_files, batch_size=batch_size)
+batch_size = 32
+
+all_files = os.listdir('images')
+shuffle(all_files)
+
+split = int(0.95 * len(all_files))
+
+# split into training and testing
+train_files = all_files[0:split]
+test_files = all_files[split:]
+
+train_generator = image_generator(train_files, batch_size=batch_size)
+test_generator = image_generator(test_files, batch_size=batch_size)
+x, y = next(train_generator)
+plt.axis('off')
+img = x[0]
+msk = y[0].squeeze()
+msk = np.stack((msk,)*3, axis=-1)
+
+plt.imshow(np.concatenate([img, msk, img*msk], axis=1))
 # TODO:
 #  finish transporting code from
 #  "https://colab.research.google.com/github/zaidalyafeai/Notebooks/blob/master/unet.ipynb#scrollTo=PvwbmS-YTHEZ"
