ea87e6aacb4d8e2aa402f6a8a6863b73e6630cc6
topspin10 <andrewH7wei@gmail.com>
2025-04-11 21:12:33 -0700

    found out how to do the list comp.

---
+++ list_comp.py
@@ -0,0 +1,19 @@
+import random
+
+list_1 = []
+for i in range(1,11):
+    for a in range(1,11):
+        bob = random.randint(1,100)
+        if bob%2 == 0:
+            list_1.append(bob**2)
+            a = a+1
+print(list_1)
+list_2 = []
+for b in range(1,11):
+    list_2 = [random.randint(1,100)**2 for c in range(1,11)]
+    print(list_2)
+list_3 = []
+for b in range(1,11):
+    list_4 = [random.randint(1,100) for d in range(1,6)]
+    list_3 = [c**2 for c in list_4 if c%2==1]
+    print(list_3)
