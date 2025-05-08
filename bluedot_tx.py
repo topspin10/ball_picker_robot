708d902383a7c9a2ec74f049215a984f79eaf465
topspin10 <AndrewH7Wei@gmail.com>
2025-05-07 18:55:13 -0700

    finished polar coor. pi to vex. YAY!!!

--- bluedot_tx.py
+++ bluedot_tx.py
@@ -4,9 +4,11 @@ import serial
 
 ser = serial.Serial('/dev/ttyACM1')  # open serial port
 cmd = None
+
 def send_dir(pos):
     # pos.distance is a float from 0 to 1.
-    cmd = f'{int(pos.distance*100)}, {int(pos.angle)}'
+    cmd = f'{int(pos.distance*100)}, {int(pos.angle)}\n'
+    print(cmd)
     ser.write(cmd.encode('ascii'))
 
 dot = BlueDot()
