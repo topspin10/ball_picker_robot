f7a1d92cde0b889beb31c4996edc8270c258c4dc
topspin10 <AndrewH7Wei@gmail.com>
2025-05-07 18:15:03 -0700

    tweaked polar coor. pi to hub

--- bluedot_tx.py
+++ bluedot_tx.py
@@ -1,15 +1,15 @@
-from bluedot import Bluedot
+from bluedot import BlueDot
 import time
 import serial
 
-ser = serial.Serial('/dev/ttyASM1')  # open serial port
+ser = serial.Serial('/dev/ttyACM1')  # open serial port
 cmd = None
 def send_dir(pos):
     # pos.distance is a float from 0 to 1.
     cmd = f'{int(pos.distance*100)}, {int(pos.angle)}'
-    ser.write(cmd)
+    ser.write(cmd.encode('ascii'))
 
-dot = Bluedot()
+dot = BlueDot()
 dot.when_moved = send_dir
 
 while True:
