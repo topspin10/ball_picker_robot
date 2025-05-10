4f728a775ac88fe517ae1ae1d3b5e2ad11972a14
topspin10 <AndrewH7Wei@gmail.com>
2025-05-09 18:00:59 -0700

    finished code. COWABUNGA!!!

--- bluedot_tx.py
+++ bluedot_tx.py
@@ -3,7 +3,12 @@ import time
 import serial
 
 ser = serial.Serial('/dev/ttyACM1')  # open serial port; port name may change if you disconnect
-cmd = None
+
+# The functions below sends commands to the serial port
+# input: commands
+# output: nothing
+def stop():
+    ser.write('0, 0\n'.encode('ascii'))
 
 def send_dir(pos):
     # pos.distance is a float from 0 to 1.
@@ -12,7 +17,9 @@ def send_dir(pos):
     ser.write(cmd.encode('ascii'))
 
 dot = BlueDot()
+dot.when_pressed = send_dir
 dot.when_moved = send_dir
+dot.when_released = stop
 
 while True:
     time.sleep(0.3)
