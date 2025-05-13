8188d9a415cc98fb75030a03d892251ca40b3525
topspin10 <AndrewH7Wei@gmail.com>
2025-05-12 21:48:18 -0700

    tried to do error catching

--- bluedot_tx.py
+++ bluedot_tx.py
@@ -2,28 +2,35 @@ from bluedot import BlueDot
 import time
 import serial
 
-ser = serial.Serial('/dev/ttyACM1')  # open serial port; port name may change if you disconnect
-success = 0
+ser = serial.Serial('/dev/ttyACM2')  # open serial port; port name may change if you disconnect
+# success = 0
 
 # The functions below sends commands to the serial port
 # input: commands
 # output: nothing
 def stop():
-    while success == 0:
-        try:
-            ser.write('0, 0\n'.encode('ascii'))
-        except:
-            ser = serial.Serial('/dev/ttyACM1')
+    # while success == 0:
+    try:
+        ser.write('0, 0\n'.encode('ascii'))
+    except:
+        ser = serial.Serial('/dev/ttyACM2')
+        ser.write('0, 0\n'.encode('ascii'))
+        # else:
+        #     success = 1
 
 
 def send_dir(pos):
     # pos.distance is a float from 0 to 1.
     cmd = f'{int(pos.distance*100)}, {int(pos.angle)}\n' # REMEMBER: \n is CRITICAL. Vex hub NEEDS it.
     # print(cmd)
+    # while success == 0:
     try:
         ser.write(cmd.encode('ascii'))
     except:
-        ser = serial.Serial('/dev/ttyACM1')
+        ser = serial.Serial('/dev/ttyACM2')
+        ser.write(cmd.encode('ascii'))
+        # else:
+        #     success = 1
 
 dot = BlueDot()
 dot.when_pressed = send_dir
