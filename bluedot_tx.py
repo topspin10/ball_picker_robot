5427a514b8124db784dde14287da914a02c9dbdc
topspin10 <AndrewH7Wei@gmail.com>
2025-05-16 18:49:29 -0700

    adjusted code. needs debugging

--- bluedot_tx.py
+++ bluedot_tx.py
@@ -1,8 +1,9 @@
 from bluedot import BlueDot
 import time
 import serial
+import sys
 
-ACM = '/dev/ttyACM1'
+ACM = sys.argv[1]
 ser = serial.Serial(ACM)  # open serial port; port name may change if you disconnect
 
 # input: command
@@ -16,6 +17,7 @@ def command_sender(d, a):
         try:
             if not success:
                 ser = serial.Serial(ACM)
+            # print(d, a)
             ser.write(f'{d}, {a}\n'.encode('ascii'))
             break
         except:
@@ -32,14 +34,21 @@ def stop():
 
 def send_dir(pos):
     # pos.distance is a float from 0 to 1.
-    d = {int(pos.distance*100)}
-    a = {int(pos.angle)}
+    d = int(pos.distance*100)
+    a = int(pos.angle)
     command_sender(d, a)
 
 dot = BlueDot()
-dot.when_pressed = send_dir
-dot.when_moved = send_dir
-dot.when_released = stop
+# dot.when_pressed = send_dir
+# dot.when_moved = send_dir
+# dot.when_released = stop
 
 while True:
-    time.sleep(0.3)
+    # time.sleep(0.3)
+    if dot.is_pressed:
+        d = int(dot.position.distance*100)
+        a = int(dot.position.angle)
+        command_sender(d, a)
+    else:
+        command_sender(0,0)
+
