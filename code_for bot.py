62ca048d6831c3eb98a1df5810df9e6bcfd9f521
unfinishedmakerspace <142112870+unfinishedmakerspace@users.noreply.github.com>
2025-05-23 18:09:32 -0700

    Update code_for bot.py

--- code_for bot.py
+++ code_for bot.py
@@ -4,4 +4,4 @@ import serial
 
 ACM = sys.argv[1]
 ser = serial.Serial(ACM)  # open serial port; port name may change if you disconnect
-ser.write(''.encode('ascii'))
+ser.write('1'.encode('ascii'))
