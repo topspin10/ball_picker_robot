9bdb9fdab6b0a4c3bbda5feb2becc01f683a056e
unfinishedmakerspace <142112870+unfinishedmakerspace@users.noreply.github.com>
2025-05-23 18:06:37 -0700

    started final code for pi

---
+++ code_for bot.py
@@ -0,0 +1,7 @@
+from picamera2 import Picamera2
+import sys
+import serial
+
+ACM = sys.argv[1]
+ser = serial.Serial(ACM)  # open serial port; port name may change if you disconnect
+ser.write(''.encode('ascii'))
