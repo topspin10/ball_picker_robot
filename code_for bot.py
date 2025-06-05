6ed4fcc51f1d878007dbc0beb0c1faf0575da9a2
topspin10 <AndrewH7Wei@gmail.com>
2025-06-04 17:58:09 -0700

    did some work on code for bot. tested code multiple times. still needs work

--- code_for bot.py
+++ code_for bot.py
@@ -1,4 +1,4 @@
-from picamera2 import Picamera2
+#from picamera2 import Picamera2
 import sys
 import serial
 import time
@@ -13,29 +13,33 @@ def command_sender(*args):
     while num_of_tries < 4:
         try:
             if not success:
-                ser = serial.Serial(ACM)
+                ser = serial.Serial(ACM,timeout=1)
             # print(d, a)
             ser.write(f'{cmd}\n'.encode('ascii'))
-            # TODO wait for okay
-            while ser.readlines() == None:
-                time.sleep(0.1)
-            # TODO wait for done to make sure the vex robot has turned (consider waiting a max amount of secs)
-            while ser.readlines() == None:
-                time.sleep(0.1)
+            start = time.time_ns()
+            """wait for cmd+ DONE at most 5 seconds"""
+            response = f"{cmd} DONE"            
+            while time.time_ns()-start < 15000000001:
+                received_response=ser.readline()
+                if received_response==response:
+                    break
+            print(time.time_ns()-start)
             break
         except:
             success = False
         num_of_tries = num_of_tries + 1
 
-picam2 = Picamera2()
-picam2.start()
+#picam2 = Picamera2()
+#picam2.start()
 ACM = sys.argv[1]
-ser = serial.Serial(ACM)  # open serial port; port name may change if you disconnect
+ser = serial.Serial(ACM,timeout=1)  # open serial port; port name may change if you disconnect
+#picam2.stop()
+#config = picam2.create_still_configuration(main={"size": (4608, 2592)})
+#picam2.configure(config)
 for p in range(12):
     command_sender('t', 30)
-    time.sleep(3)
-    config = picam2.create_still_configuration(main={"size": (4608, 2592)})
-    picam2.configure(config)
     timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
     filename = f"/home/wonwong/Projects/data/{timestamp}.jpg"
-    picam2.capture_file(filename)
+    #picam2.capture_file(filename)
+    print(p)
+print("done")
