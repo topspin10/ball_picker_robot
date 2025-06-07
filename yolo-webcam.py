9d5a95ff5973c38dc9d5263e0d9c1e210080baed
topspin10 <andrewH7wei@gmail.com>
2025-06-06 18:59:16 -0700

    working on next step of code.
    coding for webcam on laptop.

--- yolo-webcam.py
+++ yolo-webcam.py
@@ -1,28 +1,28 @@
 import cv2
-from picamera2 import Picamera2
 from ultralytics import YOLO
 
-# Initialize the Picamera2
-picam2 = Picamera2()
-picam2.preview_configuration.main.size = (1280, 720)
-picam2.preview_configuration.main.format = "RGB888"
-picam2.preview_configuration.align()
-picam2.configure("preview")
-picam2.start()
+cap = cv2.VideoCapture(0)
 
 # Load the YOLO11 model montagem tomada
-model = YOLO("/home/wonwong/Downloads/yolo11n.pt")
+model = YOLO("C:\\Users\\unfin\\Downloads\\yolo11n.pt")
 
 while True:
     # Capture frame-by-frame
-    frame = picam2.capture_array()
+    ret, frame = cap.read()
 
     # Run YOLO11 inference on the frame
     results = model(frame)
 
     # Visualize the results on the frame
     annotated_frame = results[0].plot()
-    #print(results[0].tojson())
+    max_area = 0
+
+    for i in results[0].boxes:
+        box_num = results[0].boxes[i-1].xyxy.numpy()[0]
+        area = abs(box_num[0] - box_num[2]) * abs(box_num[1] - box_num[3])
+        if max_area < area:
+            max_area = area
+    # print(results[0].tojson())
     # Display the resulting frame
     cv2.imshow("Camera", annotated_frame)
 
